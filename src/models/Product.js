6ad506801b4f8208f7c3879e37c8cd06bcd91d91
VanNgan <nguyenvanngan.0307@gmail.com>
2025-11-09 14:02:35 +0700

    Model Product complete

--- src/models/Product.js
+++ src/models/Product.js
@@ -3,5 +3,16 @@ const mongoose = require("mongoose");
 const productSchema = new mongoose.Schema({
     name : {type: String , require:true},
     slug: {type: String , require: true},
-    
-})
+    description : {type : String},
+    price : {type : Number, require : true},
+    salePrice :{type: Number},
+    brand :{type: String},
+    categories: [{type : mongoose.Schema.Types.ObjectId,ref : Category}],
+    image: [{type:String}],
+    stock:{type : Number , default: 0},
+    specs:{type: Object},
+},{timestamps:true});
+
+const Product = mongoose.model("Product",productSchema);
+module.export = Product;
+
